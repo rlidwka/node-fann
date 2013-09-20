d44c766833949a8eee52dffc61470fedd72a76c8
Alex Kocharin <alex@kocharin.ru>
2013-09-20 10:25:16 +0400

    test for saving and loading network

---
+++ tests/save_load.js
@@ -0,0 +1,10 @@
+
+var fann = require('../build/Release/fann');
+var assert = require('assert');
+var net = new fann.standard(~~(Math.random()*10),~~(Math.random()*10),~~(Math.random()*10));
+var filename = ''+Math.random()+'.net';
+net.save(filename);
+var net2 = new fann.load(filename);
+assert.deepEqual(net.layers, net2.layers);
+require('fs').unlink(filename);
+
